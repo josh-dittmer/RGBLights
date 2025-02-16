81d642d9ac6d910ddd0f6b8dbc7ca7d8d8139560
Josh Dittmer <joshua.dittmer@sjsu.edu>
2025-02-15 23:03:23 -0800

    zengge testing

--- src/drivers/zengge_driver.cpp
+++ src/drivers/zengge_driver.cpp
@@ -1,17 +1,19 @@
 #include "zengge_driver.h"
 
 bool ZenggeDriver::init() {
-    GDBusConnection* dbus_conn =
+    GDBusConnection* dbus_conn_ptr =
         g_bus_get_sync(G_BUS_TYPE_SYSTEM, nullptr, nullptr);
 
-    if (!dbus_conn) {
+    if (!dbus_conn_ptr) {
         get_logger().error("Failed to connect to system bus!");
         return false;
     }
 
-    // test
-
     get_logger().verbose("Successfully connected to system bus!");
+
+    GMainLoop* main_loop_ptr = g_main_loop_new(nullptr, false);
+
+    
 
     return true;
 }
