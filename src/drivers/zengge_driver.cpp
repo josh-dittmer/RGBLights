f5b0a85c9f26a5194e1c74eeffa61e5a51a7b635
Josh Dittmer <joshua.dittmer@sjsu.edu>
2025-02-16 07:42:28 +0000

    zengge testing

--- src/drivers/zengge_driver.cpp
+++ src/drivers/zengge_driver.cpp
@@ -1,19 +1,26 @@
 #include "zengge_driver.h"
 
-bool ZenggeDriver::init() {
-    GDBusConnection* dbus_conn_ptr =
-        g_bus_get_sync(G_BUS_TYPE_SYSTEM, nullptr, nullptr);
+const std::string ZenggeDriver::BUS_NAME = "org.bluez";
+const std::string ZenggeDriver::ADAPTER_PATH = "/org/bluez/hci0";
+const std::string ZenggeDriver::INTERFACE_NAME = "org.bluez.Adapter1";
 
-    if (!dbus_conn_ptr) {
+bool ZenggeDriver::init() {
+    m_dbus_conn_ptr.reset(g_bus_get_sync(G_BUS_TYPE_SYSTEM, nullptr, nullptr));
+
+    if (!m_dbus_conn_ptr) {
         get_logger().error("Failed to connect to system bus!");
         return false;
     }
 
     get_logger().verbose("Successfully connected to system bus!");
 
-    GMainLoop* main_loop_ptr = g_main_loop_new(nullptr, false);
+    if (!set_adapter_property("Powered", g_variant_new("b", true))) {
+        get_logger().error("Failed to enable Bluetooth adapter!");
+        return false;
+    }
 
-    
+    GMainLoop* main_loop_ptr = g_main_loop_new(nullptr, false);
+    g_main_loop_run(main_loop_ptr);
 
     return true;
 }
@@ -21,3 +28,20 @@ bool ZenggeDriver::init() {
 void ZenggeDriver::write(uint8_t r, uint8_t g, uint8_t b) {}
 
 void ZenggeDriver::shutdown() {}
+
+bool ZenggeDriver::set_adapter_property(std::string property, GVariant* value) {
+    GError* err = nullptr;
+
+    std::unique_ptr<GVariant, deleter_of<GVariant>> res_ptr;
+    res_ptr.reset(g_dbus_connection_call_sync(
+        m_dbus_conn_ptr.get(), BUS_NAME.c_str(), ADAPTER_PATH.c_str(),
+        "org.freedesktop.DBus.Properties", "Set",
+        g_variant_new("(ssv)", INTERFACE_NAME.c_str(), property, value),
+        nullptr, G_DBUS_CALL_FLAGS_NONE, -1, nullptr, &err));
+
+    if (err != nullptr) {
+        return false;
+    }
+
+    return true;
+}
--- src/drivers/zengge_driver.h
+++ src/drivers/zengge_driver.h
@@ -6,6 +6,16 @@
 
 #include "driver.h"
 
+template <class T> struct deleter_of;
+
+template <> struct deleter_of<GDBusConnection> {
+    void operator()(GDBusConnection* p) const { g_object_unref(p); }
+};
+
+template <> struct deleter_of<GVariant> {
+    void operator()(GVariant* p) const { g_variant_unref(p); }
+};
+
 class ZenggeDriver : public Driver {
   public:
     ZenggeDriver() : Driver("ZenggeDriver") {}
@@ -16,4 +26,12 @@ class ZenggeDriver : public Driver {
     void shutdown() override;
 
   private:
+    static const std::string BUS_NAME;
+    static const std::string ADAPTER_PATH; // should be found in runtime
+    static const std::string INTERFACE_NAME;
+
+    bool set_adapter_property(std::string property, GVariant* value);
+
+    std::unique_ptr<GDBusConnection, deleter_of<GDBusConnection>>
+        m_dbus_conn_ptr;
 };
