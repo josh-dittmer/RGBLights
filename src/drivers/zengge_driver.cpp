b4e02e4d7680244fd084095f3171197e5921cbe4
Josh Dittmer <joshua.dittmer@sjsu.edu>
2025-02-20 06:18:12 +0000

    zengge testing

--- src/drivers/zengge_driver.cpp
+++ src/drivers/zengge_driver.cpp
@@ -1,8 +1,9 @@
 #include "zengge_driver.h"
 
+#include <cassert>
+
 const std::string ZenggeDriver::BUS_NAME = "org.bluez";
 const std::string ZenggeDriver::ADAPTER_PATH = "/org/bluez/hci0";
-const std::string ZenggeDriver::INTERFACE_NAME = "org.bluez.Adapter1";
 
 bool ZenggeDriver::init() {
     m_dbus_conn_ptr.reset(g_bus_get_sync(G_BUS_TYPE_SYSTEM, nullptr, nullptr));
@@ -13,6 +14,12 @@ bool ZenggeDriver::init() {
     }
 
     get_logger().verbose("Successfully connected to system bus!");
+
+    guint adapter_change_signal_id = g_dbus_connection_signal_subscribe(
+        m_dbus_conn_ptr.get(), BUS_NAME.c_str(),
+        "org.freedesktop.DBus.Properties", "PropertiesChanged", nullptr,
+        "org.bluez.Adapter1", G_DBUS_SIGNAL_FLAGS_NONE,
+        ZenggeDriver::adapter_change_cb, this, nullptr);
 
     if (!set_adapter_property("Powered", g_variant_new("b", true))) {
         get_logger().error("Failed to enable Bluetooth adapter!");
@@ -44,17 +51,96 @@ void ZenggeDriver::shutdown() {
     get_logger().log("Driver shutdown successfully!");
 }
 
-bool ZenggeDriver::set_adapter_property(std::string property, GVariant* value) {
-    GError* err = nullptr;
+void ZenggeDriver::adapter_change_cb(GDBusConnection* dbus_conn,
+                                     const gchar* sender,
+                                     const gchar* object_path,
+                                     const gchar* interface,
+                                     const gchar* signal, GVariant* params,
+                                     void* user_data) {
+    assert(user_data != nullptr);
+    ZenggeDriver* instance = static_cast<ZenggeDriver*>(user_data);
 
-    std::unique_ptr<GVariant, deleter_of<GVariant>> res_ptr;
+    instance->get_logger().verbose(
+        "adapter_change_cb(): Adapter change callback invoked");
+
+    g_unique_ptr<GVariant> changed_properties_variant_ptr;
+    changed_properties_variant_ptr.reset(g_variant_get_child_value(params, 1));
+
+    if (!changed_properties_variant_ptr) {
+        instance->get_logger().warn(
+            "adapter_change_cb(): Failed to read adapter properties!");
+        return;
+    }
+
+    g_unique_ptr<GVariant> discovering_variant_ptr;
+    discovering_variant_ptr.reset(g_variant_lookup_value(
+        changed_properties_variant_ptr.get(), "Discovering", nullptr));
+
+    g_unique_ptr<GVariant> powered_variant_ptr;
+    powered_variant_ptr.reset(g_variant_lookup_value(
+        changed_properties_variant_ptr.get(), "Powered", nullptr));
+
+    if (discovering_variant_ptr) {
+        if (!g_variant_is_of_type(discovering_variant_ptr.get(),
+                                  G_VARIANT_TYPE_BOOLEAN)) {
+            instance->get_logger().debug("adapter_change_cb(): \"Discovering\" "
+                                         "variant has invalid type");
+            return;
+        }
+
+        const bool scanning =
+            g_variant_get_boolean(discovering_variant_ptr.get());
+
+        instance->get_logger().log("Bluetooth adapter change: Adapter is " +
+                                           scanning
+                                       ? "now scanning"
+                                       : "no longer scanning");
+    }
+
+    if (powered_variant_ptr) {
+        if (!g_variant_is_of_type(powered_variant_ptr.get(),
+                                  G_VARIANT_TYPE_BOOLEAN)) {
+            instance->get_logger().debug(
+                "adapter_change_cb(): \"Powered\" variant has invalid type");
+            return;
+        }
+
+        const bool powered = g_variant_get_boolean(powered_variant_ptr.get());
+
+        instance->get_logger().log(
+            "Bluetooth adapter change: Adapter is now powered " + powered
+                ? "ON"
+                : "OFF");
+    }
+}
+
+void ZenggeDriver::device_appear_cb(GDBusConnection* dbus_conn,
+                                    const gchar* sender,
+                                    const gchar* object_path,
+                                    const gchar* interface, const gchar* signal,
+                                    GVariant* params, void* user_data) {}
+
+void ZenggeDriver::device_disappear_cb(GDBusConnection* dbus_conn,
+                                       const gchar* sender,
+                                       const gchar* object_path,
+                                       const gchar* interface,
+                                       const gchar* signal, GVariant* params,
+                                       void* user_data) {}
+
+bool ZenggeDriver::set_adapter_property(std::string property, GVariant* value) {
+    GError* err_raw_ptr = nullptr;
+
+    g_unique_ptr<GVariant> res_ptr;
     res_ptr.reset(g_dbus_connection_call_sync(
         m_dbus_conn_ptr.get(), BUS_NAME.c_str(), ADAPTER_PATH.c_str(),
         "org.freedesktop.DBus.Properties", "Set",
-        g_variant_new("(ssv)", INTERFACE_NAME.c_str(), property.c_str(), value),
-        nullptr, G_DBUS_CALL_FLAGS_NONE, -1, nullptr, &err));
+        g_variant_new("(ssv)", "org.bluez.Adapter1", property.c_str(), value),
+        nullptr, G_DBUS_CALL_FLAGS_NONE, -1, nullptr, &err_raw_ptr));
 
-    if (err != nullptr) {
+    g_unique_ptr<GError> err_ptr;
+    err_ptr.reset(err_raw_ptr);
+
+    if (err_ptr) {
         return false;
     }
 
--- src/drivers/zengge_driver.h
+++ src/drivers/zengge_driver.h
@@ -21,6 +21,17 @@ template <> struct deleter_of<GVariant> {
     void operator()(GVariant* p) const { g_variant_unref(p); }
 };
 
+template <> struct deleter_of<GVariantIter> {
+    void operator()(GVariantIter* p) const { g_variant_iter_free(p); }
+};
+
+template <> struct deleter_of<GError> {
+    void operator()(GError* p) const { g_error_free(p); }
+};
+
+template <class g_type>
+using g_unique_ptr = std::unique_ptr<g_type, deleter_of<g_type>>;
+
 class ZenggeDriver : public Driver {
   public:
     ZenggeDriver() : Driver("ZenggeDriver") {}
@@ -33,14 +44,26 @@ class ZenggeDriver : public Driver {
   private:
     static const std::string BUS_NAME;
     static const std::string ADAPTER_PATH; // should be found in runtime
-    static const std::string INTERFACE_NAME;
+
+    static void adapter_change_cb(GDBusConnection* dbus_conn,
+                                  const gchar* sender, const gchar* object_path,
+                                  const gchar* interface, const gchar* signal,
+                                  GVariant* params, void* user_data);
+    static void device_appear_cb(GDBusConnection* dbus_conn,
+                                 const gchar* sender, const gchar* object_path,
+                                 const gchar* interface, const gchar* signal,
+                                 GVariant* params, void* user_data);
+    static void device_disappear_cb(GDBusConnection* dbus_conn,
+                                    const gchar* sender,
+                                    const gchar* object_path,
+                                    const gchar* interface, const gchar* signal,
+                                    GVariant* params, void* user_data);
 
     bool set_adapter_property(std::string property, GVariant* value);
 
     std::thread m_loop_thread;
 
-    std::unique_ptr<GDBusConnection, deleter_of<GDBusConnection>>
-        m_dbus_conn_ptr;
+    g_unique_ptr<GDBusConnection> m_dbus_conn_ptr;
 
-    std::unique_ptr<GMainLoop, deleter_of<GMainLoop>> m_g_loop_ptr;
+    g_unique_ptr<GMainLoop> m_g_loop_ptr;
 };
